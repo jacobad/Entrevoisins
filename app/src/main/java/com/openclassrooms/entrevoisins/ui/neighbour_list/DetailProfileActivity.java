0fcf1a404751a762f28dfa525b060251283c041a
jacobad <45879304+jacobad@users.noreply.github.com>
2021-01-17 21:25:27 +0100

    4 ... commit

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailProfileActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailProfileActivity.java
@@ -11,7 +11,7 @@ public class DetailProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_profile);
-        getSupportActionBar().setTitle("...");
+        getSupportActionBar().setTitle("....");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
