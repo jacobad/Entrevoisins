0db57401cabf46115dc1242b75c0fb333c5e3d3c
jacobad <45879304+jacobad@users.noreply.github.com>
2021-02-11 03:02:44 +0100

    test finish

---
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivityTest.java
@@ -0,0 +1,50 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.openclassrooms.entrevoisins.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ListNeighbourActivityTest {
+
+
+    @Rule
+    public ActivityTestRule<ListNeighbourActivity> mActivityTestRule = new ActivityTestRule<>(ListNeighbourActivity.class);
+
+    @Test
+    public void launchDetailstNeighbourActivityTest() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.list_neighbours),
+                        withParent(withId(R.id.container))));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
+
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.mDetailName), withText("Caroline"),
+                        withParent(allOf(withId(R.id.linearLayoutDetail2),
+                                withParent(withId(R.id.detail_text)))),
+                        isDisplayed()));
+        textView.check(matches(isDisplayed()));
+
+
+    }
+}
